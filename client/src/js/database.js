33eacb567400f6441adf4a44b8460a5e17180f93
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-14 13:49:10 -0800

    Added database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Accepts the content and add it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to database.', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Get all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Data retrieved.', result);
+};
 
 initdb();
